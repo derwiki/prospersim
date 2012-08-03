90c9526291eea760ffd1c83ba9810ff1ce9c8feb
Adam Derewecki <adam@causes.com>
2012-08-02 23:14:16 -0700

    First naive version

--- prosper.rb
+++ prosper.rb
@@ -4,11 +4,7 @@ monthly = total * return_rate / 36.0
 puts "Monthly capital available: #{monthly}"
 
 long_gain = 0
-minor_bump = 0
 36.times do
-  long_gain += (monthly + minor_bump) * return_rate
-
-  minor_bump += (monthly * return_rate) / 36.0
-  puts "Minor bump is: #{minor_bump}"
+  long_gain += monthly * return_rate
 end
-puts "Reinvesting dividends will return longterm: #{long_gain}"
+puts "Reinvesting naively will return longterm: #{long_gain}"
