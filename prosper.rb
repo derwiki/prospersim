f62cdbaf82e09297f09f4cf631d812780acdf99b
Adam Derewecki <adam@causes.com>
2012-08-02 23:14:46 -0700

    Reinvesting dividends

--- prosper.rb
+++ prosper.rb
@@ -4,7 +4,11 @@ monthly = total * return_rate / 36.0
 puts "Monthly capital available: #{monthly}"
 
 long_gain = 0
+minor_bump = 0
 36.times do
-  long_gain += monthly * return_rate
+  long_gain += (monthly + minor_bump) * return_rate
+
+  minor_bump += (monthly * return_rate) / 36.0
+  puts "Minor bump is: #{minor_bump}"
 end
-puts "Reinvesting naively will return longterm: #{long_gain}"
+puts "Reinvesting dividends will return longterm: #{long_gain}"
